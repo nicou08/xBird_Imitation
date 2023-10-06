a1c09a0b75c64e0a040154076158d26c311c1f75
nicou08 <nicolas.guasca16@gmail.com>
2023-10-06 03:41:18 -0600

    Update serverAuth.ts

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -6,8 +6,7 @@ import { authOptions } from "./authOpt";
 const serverAuth = async () => {
   const session = await getServerSession(authOptions);
   console.log("serverAuth::session", session);
-  console.log("Hello from serverAuth");
-  console.log("MONGODB_URI", process.env.MONGODB_URI);
+  console.log("Hello from serverAuthh");
   console.log("serverAuth:prismadb", prisma);
   console.log("What is good");
   if (!session?.user?.email) {
